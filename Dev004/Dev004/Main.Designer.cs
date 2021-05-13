840e91dba57ba1cd0815c552122dc202ed95875f
JBD <lio222@hanmail.net>
2021-05-14 08:32:27 +0900

    2021-05-14 DevExpress  Adorner UI Manager & Badges

---
+++ Dev004/Dev004/Main.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace Dev004
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.ribbon = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.barBtnContact = new DevExpress.XtraBars.BarButtonItem();
+            this.barBtnMail = new DevExpress.XtraBars.BarButtonItem();
+            this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonStatusBar = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
+            this.documentManager1 = new DevExpress.XtraBars.Docking2010.DocumentManager(this.components);
+            this.tabbedView1 = new DevExpress.XtraBars.Docking2010.Views.Tabbed.TabbedView(this.components);
+            this.adornerUIManager1 = new DevExpress.Utils.VisualEffects.AdornerUIManager(this.components);
+            this.badge1 = new DevExpress.Utils.VisualEffects.Badge();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ribbon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.documentManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabbedView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adornerUIManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbon
+            // 
+            this.ribbon.ExpandCollapseItem.Id = 0;
+            this.ribbon.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbon.ExpandCollapseItem,
+            this.ribbon.SearchEditItem,
+            this.barBtnContact,
+            this.barBtnMail});
+            this.ribbon.Location = new System.Drawing.Point(0, 0);
+            this.ribbon.MaxItemId = 3;
+            this.ribbon.Name = "ribbon";
+            this.ribbon.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1});
+            this.ribbon.Size = new System.Drawing.Size(851, 160);
+            this.ribbon.StatusBar = this.ribbonStatusBar;
+            // 
+            // barBtnContact
+            // 
+            this.barBtnContact.Caption = "Contact";
+            this.barBtnContact.Id = 1;
+            this.barBtnContact.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("barBtnContact.ImageOptions.Image")));
+            this.barBtnContact.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("barBtnContact.ImageOptions.LargeImage")));
+            this.barBtnContact.Name = "barBtnContact";
+            // 
+            // barBtnMail
+            // 
+            this.barBtnMail.Caption = "Mail";
+            this.barBtnMail.Id = 2;
+            this.barBtnMail.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("barBtnMail.ImageOptions.Image")));
+            this.barBtnMail.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("barBtnMail.ImageOptions.LargeImage")));
+            this.barBtnMail.Name = "barBtnMail";
+            this.barBtnMail.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barBtnMail_ItemClick);
+            // 
+            // ribbonPage1
+            // 
+            this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup1});
+            this.ribbonPage1.Name = "ribbonPage1";
+            this.ribbonPage1.Text = "Dev Express";
+            // 
+            // ribbonPageGroup1
+            // 
+            this.ribbonPageGroup1.ItemLinks.Add(this.barBtnContact);
+            this.ribbonPageGroup1.ItemLinks.Add(this.barBtnMail);
+            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+            this.ribbonPageGroup1.Text = "ribbonPageGroup1";
+            // 
+            // ribbonStatusBar
+            // 
+            this.ribbonStatusBar.Location = new System.Drawing.Point(0, 472);
+            this.ribbonStatusBar.Name = "ribbonStatusBar";
+            this.ribbonStatusBar.Ribbon = this.ribbon;
+            this.ribbonStatusBar.Size = new System.Drawing.Size(851, 24);
+            // 
+            // documentManager1
+            // 
+            this.documentManager1.MdiParent = this;
+            this.documentManager1.MenuManager = this.ribbon;
+            this.documentManager1.View = this.tabbedView1;
+            this.documentManager1.ViewCollection.AddRange(new DevExpress.XtraBars.Docking2010.Views.BaseView[] {
+            this.tabbedView1});
+            // 
+            // adornerUIManager1
+            // 
+            this.adornerUIManager1.Elements.Add(this.badge1);
+            this.adornerUIManager1.Owner = this;
+            // 
+            // badge1
+            // 
+            this.badge1.Properties.Location = System.Drawing.ContentAlignment.TopRight;
+            this.badge1.TargetElement = this.barBtnContact;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 496);
+            this.Controls.Add(this.ribbonStatusBar);
+            this.Controls.Add(this.ribbon);
+            this.IsMdiContainer = true;
+            this.Name = "Main";
+            this.Ribbon = this.ribbon;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StatusBar = this.ribbonStatusBar;
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ribbon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.documentManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabbedView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adornerUIManager1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbon;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+        private DevExpress.XtraBars.Ribbon.RibbonStatusBar ribbonStatusBar;
+        private DevExpress.XtraBars.BarButtonItem barBtnContact;
+        private DevExpress.XtraBars.BarButtonItem barBtnMail;
+        private DevExpress.XtraBars.Docking2010.DocumentManager documentManager1;
+        private DevExpress.XtraBars.Docking2010.Views.Tabbed.TabbedView tabbedView1;
+        private DevExpress.Utils.VisualEffects.AdornerUIManager adornerUIManager1;
+        private DevExpress.Utils.VisualEffects.Badge badge1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Dev004/Dev004/Main.cs
@@ -0,0 +1,50 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dev004
+{
+    public partial class Main : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        static int mail = 0; 
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            badge1.Properties.Text = (++mail).ToString();
+            badge1.Visible = true;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            badge1.Visible = false;
+            timer1.Start();
+        }
+
+        private void barBtnMail_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            if (mail == 0)
+                return;
+                badge1.Properties.Text = (--mail).ToString();
+            if (mail == 0)
+                badge1.Visible = false; 
+            XtraForm1 frm = new XtraForm1();
+            frm.MdiParent = this;
+            frm.Show();
+
+
+
+        }
+    }
+}
