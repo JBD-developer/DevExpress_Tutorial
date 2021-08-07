7de73240486ddfdd745ef42e009bdb2bbf04bfdf
JBD <lio222@hanmail.net>
2021-08-07 16:10:10 +0900

    2021-08-07 DevExpress QRCode

---
+++ Dev009/Dev009_2/Form.Designer.cs
@@ -0,0 +1,331 @@
+﻿
+namespace Dev009_2
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            DevExpress.XtraLayout.ColumnDefinition columnDefinition1 = new DevExpress.XtraLayout.ColumnDefinition();
+            DevExpress.XtraLayout.RowDefinition rowDefinition1 = new DevExpress.XtraLayout.RowDefinition();
+            DevExpress.XtraLayout.RowDefinition rowDefinition2 = new DevExpress.XtraLayout.RowDefinition();
+            this.ltc = new DevExpress.XtraLayout.LayoutControl();
+            this.pieQRcodePreview = new DevExpress.XtraEditors.PictureEdit();
+            this.btnSave = new DevExpress.XtraEditors.SimpleButton();
+            this.btnCreate = new DevExpress.XtraEditors.SimpleButton();
+            this.btnClear = new DevExpress.XtraEditors.SimpleButton();
+            this.comboBoxEdit1 = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.txtQRCode = new DevExpress.XtraEditors.TextEdit();
+            this.ltg = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem5 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.layoutControlGroup2 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem6 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.ltc)).BeginInit();
+            this.ltc.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pieQRcodePreview.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtQRCode.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ltg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ltc
+            // 
+            this.ltc.Controls.Add(this.pieQRcodePreview);
+            this.ltc.Controls.Add(this.btnSave);
+            this.ltc.Controls.Add(this.btnCreate);
+            this.ltc.Controls.Add(this.btnClear);
+            this.ltc.Controls.Add(this.comboBoxEdit1);
+            this.ltc.Controls.Add(this.txtQRCode);
+            this.ltc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ltc.Location = new System.Drawing.Point(0, 0);
+            this.ltc.Name = "ltc";
+            this.ltc.Root = this.ltg;
+            this.ltc.Size = new System.Drawing.Size(474, 515);
+            this.ltc.TabIndex = 0;
+            this.ltc.Text = "layoutControl1";
+            // 
+            // pieQRcodePreview
+            // 
+            this.pieQRcodePreview.Location = new System.Drawing.Point(7, 160);
+            this.pieQRcodePreview.Name = "pieQRcodePreview";
+            this.pieQRcodePreview.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pieQRcodePreview.Size = new System.Drawing.Size(460, 348);
+            this.pieQRcodePreview.StyleController = this.ltc;
+            this.pieQRcodePreview.TabIndex = 9;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Appearance.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold);
+            this.btnSave.Appearance.Options.UseFont = true;
+            this.btnSave.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.ImageOptions.Image")));
+            this.btnSave.Location = new System.Drawing.Point(342, 92);
+            this.btnSave.MaximumSize = new System.Drawing.Size(125, 35);
+            this.btnSave.MinimumSize = new System.Drawing.Size(125, 35);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(125, 35);
+            this.btnSave.StyleController = this.ltc;
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Appearance.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold);
+            this.btnCreate.Appearance.Options.UseFont = true;
+            this.btnCreate.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnCreate.ImageOptions.Image")));
+            this.btnCreate.Location = new System.Drawing.Point(213, 92);
+            this.btnCreate.MaximumSize = new System.Drawing.Size(125, 35);
+            this.btnCreate.MinimumSize = new System.Drawing.Size(125, 35);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(125, 35);
+            this.btnCreate.StyleController = this.ltc;
+            this.btnCreate.TabIndex = 7;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Appearance.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold);
+            this.btnClear.Appearance.Options.UseFont = true;
+            this.btnClear.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnClear.ImageOptions.Image")));
+            this.btnClear.Location = new System.Drawing.Point(84, 92);
+            this.btnClear.MaximumSize = new System.Drawing.Size(125, 35);
+            this.btnClear.MinimumSize = new System.Drawing.Size(125, 35);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(125, 35);
+            this.btnClear.StyleController = this.ltc;
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // comboBoxEdit1
+            // 
+            this.comboBoxEdit1.Location = new System.Drawing.Point(102, 60);
+            this.comboBoxEdit1.Name = "comboBoxEdit1";
+            this.comboBoxEdit1.Properties.Appearance.Font = new System.Drawing.Font("맑은 고딕", 12F);
+            this.comboBoxEdit1.Properties.Appearance.Options.UseFont = true;
+            this.comboBoxEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.comboBoxEdit1.Size = new System.Drawing.Size(365, 28);
+            this.comboBoxEdit1.StyleController = this.ltc;
+            this.comboBoxEdit1.TabIndex = 5;
+            // 
+            // txtQRCode
+            // 
+            this.txtQRCode.Location = new System.Drawing.Point(102, 28);
+            this.txtQRCode.Name = "txtQRCode";
+            this.txtQRCode.Properties.Appearance.Font = new System.Drawing.Font("맑은 고딕", 12F);
+            this.txtQRCode.Properties.Appearance.Options.UseFont = true;
+            this.txtQRCode.Size = new System.Drawing.Size(365, 28);
+            this.txtQRCode.StyleController = this.ltc;
+            this.txtQRCode.TabIndex = 4;
+            // 
+            // ltg
+            // 
+            this.ltg.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.ltg.GroupBordersVisible = false;
+            this.ltg.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlGroup1,
+            this.layoutControlGroup2});
+            this.ltg.LayoutMode = DevExpress.XtraLayout.Utils.LayoutMode.Table;
+            this.ltg.Name = "ltg";
+            columnDefinition1.SizeType = System.Windows.Forms.SizeType.Percent;
+            columnDefinition1.Width = 100D;
+            this.ltg.OptionsTableLayoutGroup.ColumnDefinitions.AddRange(new DevExpress.XtraLayout.ColumnDefinition[] {
+            columnDefinition1});
+            rowDefinition1.Height = 132D;
+            rowDefinition1.SizeType = System.Windows.Forms.SizeType.AutoSize;
+            rowDefinition2.Height = 381D;
+            rowDefinition2.SizeType = System.Windows.Forms.SizeType.AutoSize;
+            this.ltg.OptionsTableLayoutGroup.RowDefinitions.AddRange(new DevExpress.XtraLayout.RowDefinition[] {
+            rowDefinition1,
+            rowDefinition2});
+            this.ltg.Padding = new DevExpress.XtraLayout.Utils.Padding(1, 1, 1, 1);
+            this.ltg.Size = new System.Drawing.Size(474, 515);
+            this.ltg.TextVisible = false;
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.AppearanceGroup.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold);
+            this.layoutControlGroup1.AppearanceGroup.Options.UseFont = true;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.layoutControlItem2,
+            this.layoutControlItem3,
+            this.layoutControlItem4,
+            this.layoutControlItem5,
+            this.emptySpaceItem1});
+            this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup1.Name = "layoutControlGroup1";
+            this.layoutControlGroup1.Padding = new DevExpress.XtraLayout.Utils.Padding(1, 1, 1, 1);
+            this.layoutControlGroup1.Size = new System.Drawing.Size(472, 132);
+            this.layoutControlGroup1.Text = "QRCode Information";
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.AppearanceItemCaption.Font = new System.Drawing.Font("맑은 고딕", 12F);
+            this.layoutControlItem1.AppearanceItemCaption.Options.UseFont = true;
+            this.layoutControlItem1.Control = this.txtQRCode;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(464, 32);
+            this.layoutControlItem1.Text = "QRCode ";
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(92, 21);
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.AppearanceItemCaption.Font = new System.Drawing.Font("맑은 고딕", 12F);
+            this.layoutControlItem2.AppearanceItemCaption.Options.UseFont = true;
+            this.layoutControlItem2.Control = this.comboBoxEdit1;
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 32);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(464, 32);
+            this.layoutControlItem2.Text = "Save Format";
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(92, 21);
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.btnClear;
+            this.layoutControlItem3.Location = new System.Drawing.Point(77, 64);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(129, 39);
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem3.TextVisible = false;
+            // 
+            // layoutControlItem4
+            // 
+            this.layoutControlItem4.Control = this.btnCreate;
+            this.layoutControlItem4.Location = new System.Drawing.Point(206, 64);
+            this.layoutControlItem4.Name = "layoutControlItem4";
+            this.layoutControlItem4.Size = new System.Drawing.Size(129, 39);
+            this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem4.TextVisible = false;
+            // 
+            // layoutControlItem5
+            // 
+            this.layoutControlItem5.Control = this.btnSave;
+            this.layoutControlItem5.Location = new System.Drawing.Point(335, 64);
+            this.layoutControlItem5.Name = "layoutControlItem5";
+            this.layoutControlItem5.Size = new System.Drawing.Size(129, 39);
+            this.layoutControlItem5.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem5.TextVisible = false;
+            // 
+            // emptySpaceItem1
+            // 
+            this.emptySpaceItem1.AllowHotTrack = false;
+            this.emptySpaceItem1.Location = new System.Drawing.Point(0, 64);
+            this.emptySpaceItem1.Name = "emptySpaceItem1";
+            this.emptySpaceItem1.Size = new System.Drawing.Size(77, 39);
+            this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // layoutControlGroup2
+            // 
+            this.layoutControlGroup2.AppearanceGroup.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold);
+            this.layoutControlGroup2.AppearanceGroup.Options.UseFont = true;
+            this.layoutControlGroup2.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem6});
+            this.layoutControlGroup2.Location = new System.Drawing.Point(0, 132);
+            this.layoutControlGroup2.Name = "layoutControlGroup2";
+            this.layoutControlGroup2.OptionsTableLayoutItem.RowIndex = 1;
+            this.layoutControlGroup2.Padding = new DevExpress.XtraLayout.Utils.Padding(1, 1, 1, 1);
+            this.layoutControlGroup2.Size = new System.Drawing.Size(472, 381);
+            this.layoutControlGroup2.Text = "QRCode Preview";
+            // 
+            // layoutControlItem6
+            // 
+            this.layoutControlItem6.Control = this.pieQRcodePreview;
+            this.layoutControlItem6.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem6.Name = "layoutControlItem6";
+            this.layoutControlItem6.OptionsTableLayoutItem.RowIndex = 1;
+            this.layoutControlItem6.Size = new System.Drawing.Size(464, 352);
+            this.layoutControlItem6.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem6.TextVisible = false;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 515);
+            this.Controls.Add(this.ltc);
+            this.Name = "Form";
+            this.Text = "Form";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ltc)).EndInit();
+            this.ltc.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pieQRcodePreview.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtQRCode.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ltg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem6)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl ltc;
+        private DevExpress.XtraLayout.LayoutControlGroup ltg;
+        private DevExpress.XtraEditors.PictureEdit pieQRcodePreview;
+        private DevExpress.XtraEditors.SimpleButton btnSave;
+        private DevExpress.XtraEditors.SimpleButton btnCreate;
+        private DevExpress.XtraEditors.SimpleButton btnClear;
+        private DevExpress.XtraEditors.ComboBoxEdit comboBoxEdit1;
+        private DevExpress.XtraEditors.TextEdit txtQRCode;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem4;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem5;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem6;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup2;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem1;
+    }
+}
---
+++ Dev009/Dev009_2/Form.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Drawing;
+
+using DevExpress.BarCodes;
+using DevExpress.XtraEditors;
+
+namespace Dev009_2
+{
+    public partial class Form : XtraForm
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            switch (((SimpleButton)sender).Name)
+            {
+                case "btnCreate":
+                    LF_QRcodeCreate();
+                    break;
+
+                case "btnClear":
+                    break;
+                
+                case "btnSave":
+                    break;
+                    
+                default:
+                    break;
+            }
+        }
+        private void LF_QRcodeCreate()
+        {
+            pieQRcodePreview.Image = null;
+            BarCode barCode = new BarCode();
+            barCode.Symbology = Symbology.QRCode;
+            barCode.CodeText = txtQRCode.Text;
+            barCode.BackColor = Color.White;
+            barCode.ForeColor = Color.Black;
+            barCode.RotationAngle = 0;
+            barCode.CodeBinaryData = Encoding.Default.GetBytes(barCode.CodeText);
+            barCode.Options.QRCode.CompactionMode = QRCodeCompactionMode.Byte;
+            barCode.Options.QRCode.ErrorLevel = QRCodeErrorLevel.Q;
+            barCode.Options.QRCode.ShowCodeText = false;
+            barCode.DpiX = 170;
+            barCode.DpiY = 170;
+            barCode.Module = 2f;
+            this.pieQRcodePreview.Image = barCode.BarCodeImage;
+        }
+
+
+    }
+}
