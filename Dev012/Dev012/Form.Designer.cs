31632789eeaef05a9231e5cd3dc1e5af721dbee4
JBD <lio222@hanmail.net>
2021-08-21 12:42:50 +0900

    2021-08-21 DevExpress XtraMessageBox

---
+++ Dev012/Dev012/Form.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Dev012
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMessage = new DevExpress.XtraEditors.SimpleButton();
+            this.btnExit = new DevExpress.XtraEditors.SimpleButton();
+            this.SuspendLayout();
+            // 
+            // btnMessage
+            // 
+            this.btnMessage.Location = new System.Drawing.Point(188, 204);
+            this.btnMessage.Name = "btnMessage";
+            this.btnMessage.Size = new System.Drawing.Size(96, 23);
+            this.btnMessage.TabIndex = 0;
+            this.btnMessage.Text = "Message";
+            this.btnMessage.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(188, 233);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(98, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 268);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnMessage);
+            this.Name = "Form";
+            this.Text = "Form";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton btnMessage;
+        private DevExpress.XtraEditors.SimpleButton btnExit;
+    }
+}
---
+++ Dev012/Dev012/Form.cs
@@ -0,0 +1,56 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dev012
+{
+    public partial class Form : DevExpress.XtraEditors.XtraForm
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+        private void Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LF_MessageBox()
+        {
+            XtraMessageBoxArgs messageBox = new XtraMessageBoxArgs();
+            int iTimeCount = 2000;
+            messageBox.AutoCloseOptions.Delay = iTimeCount;
+            messageBox.Caption = "확인";
+            messageBox.Text = (iTimeCount / 1000).ToString() + "초 후 자동으로 닫힙니다";
+            messageBox.Buttons = new DialogResult[] { DialogResult.OK, DialogResult.Cancel };
+
+            XtraMessageBox.Show(messageBox).ToString();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            switch (((SimpleButton)sender).Name)
+            {
+                case "btnMessage":
+                    LF_MessageBox();
+                    break;
+
+                case "btnExit":
+                    this.Close();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        
+    }
+}
