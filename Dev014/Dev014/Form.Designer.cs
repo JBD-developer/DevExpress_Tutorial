784c492153a3dedef4d1a262b4362b00aa3cda9c
JBD <lio222@hanmail.net>
2021-09-24 13:21:17 +0900

    2021-09-24 DevExpress Camera Control

---
+++ Dev014/Dev014/Form.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace Dev014
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cameraControl1 = new DevExpress.XtraEditors.Camera.CameraControl();
+            this.btnCamera = new DevExpress.XtraEditors.SimpleButton();
+            this.imageComboBoxEdit1 = new DevExpress.XtraEditors.ImageComboBoxEdit();
+            this.radioGroup = new DevExpress.XtraEditors.RadioGroup();
+            this.toggleSwitch1 = new DevExpress.XtraEditors.ToggleSwitch();
+            this.txtID = new DevExpress.XtraEditors.TextEdit();
+            this.txtPwd = new DevExpress.XtraEditors.TextEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageComboBoxEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioGroup.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toggleSwitch1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtID.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPwd.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cameraControl1
+            // 
+            this.cameraControl1.Location = new System.Drawing.Point(12, 12);
+            this.cameraControl1.Name = "cameraControl1";
+            this.cameraControl1.Size = new System.Drawing.Size(388, 440);
+            this.cameraControl1.TabIndex = 0;
+            this.cameraControl1.Text = "cameraControl1";
+            this.cameraControl1.Visible = false;
+            // 
+            // btnCamera
+            // 
+            this.btnCamera.Location = new System.Drawing.Point(427, 384);
+            this.btnCamera.Name = "btnCamera";
+            this.btnCamera.Size = new System.Drawing.Size(75, 23);
+            this.btnCamera.TabIndex = 1;
+            this.btnCamera.Text = "Camera";
+            this.btnCamera.Click += new System.EventHandler(this.Button_Cllick);
+            // 
+            // imageComboBoxEdit1
+            // 
+            this.imageComboBoxEdit1.Location = new System.Drawing.Point(427, 346);
+            this.imageComboBoxEdit1.Name = "imageComboBoxEdit1";
+            this.imageComboBoxEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.imageComboBoxEdit1.Size = new System.Drawing.Size(100, 20);
+            this.imageComboBoxEdit1.TabIndex = 2;
+            this.imageComboBoxEdit1.Visible = false;
+            this.imageComboBoxEdit1.SelectedIndexChanged += new System.EventHandler(this.LF_CameraIndex_Change);
+            // 
+            // radioGroup
+            // 
+            this.radioGroup.Location = new System.Drawing.Point(427, 124);
+            this.radioGroup.Name = "radioGroup";
+            this.radioGroup.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.radioGroup.Properties.Appearance.Options.UseBackColor = true;
+            this.radioGroup.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.radioGroup.Properties.ColumnIndent = 2;
+            this.radioGroup.Properties.Columns = 2;
+            this.radioGroup.Properties.GlyphAlignment = DevExpress.Utils.HorzAlignment.Default;
+            this.radioGroup.Properties.Items.AddRange(new DevExpress.XtraEditors.Controls.RadioGroupItem[] {
+            new DevExpress.XtraEditors.Controls.RadioGroupItem(null, "Camera1"),
+            new DevExpress.XtraEditors.Controls.RadioGroupItem(null, "Camera2")});
+            this.radioGroup.Properties.ItemsLayout = DevExpress.XtraEditors.RadioGroupItemsLayout.Column;
+            this.radioGroup.Size = new System.Drawing.Size(190, 38);
+            this.radioGroup.TabIndex = 3;
+            this.radioGroup.SelectedIndexChanged += new System.EventHandler(this.LF_Radio_CameraChange);
+            // 
+            // toggleSwitch1
+            // 
+            this.toggleSwitch1.Location = new System.Drawing.Point(427, 307);
+            this.toggleSwitch1.Name = "toggleSwitch1";
+            this.toggleSwitch1.Properties.OffText = "Off";
+            this.toggleSwitch1.Properties.OnText = "On";
+            this.toggleSwitch1.Size = new System.Drawing.Size(100, 19);
+            this.toggleSwitch1.TabIndex = 4;
+            this.toggleSwitch1.Toggled += new System.EventHandler(this.LF_Camera_Change_Toggle);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(427, 216);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 5;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(427, 262);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(100, 20);
+            this.txtPwd.TabIndex = 6;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 479);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.toggleSwitch1);
+            this.Controls.Add(this.radioGroup);
+            this.Controls.Add(this.imageComboBoxEdit1);
+            this.Controls.Add(this.btnCamera);
+            this.Controls.Add(this.cameraControl1);
+            this.Name = "Form";
+            this.Text = "Form";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Closing);
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageComboBoxEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioGroup.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toggleSwitch1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtID.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPwd.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.Camera.CameraControl cameraControl1;
+        private DevExpress.XtraEditors.SimpleButton btnCamera;
+        private DevExpress.XtraEditors.ImageComboBoxEdit imageComboBoxEdit1;
+        private DevExpress.XtraEditors.RadioGroup radioGroup;
+        private DevExpress.XtraEditors.ToggleSwitch toggleSwitch1;
+        private DevExpress.XtraEditors.TextEdit txtID;
+        private DevExpress.XtraEditors.TextEdit txtPwd;
+    }
+}
---
+++ Dev014/Dev014/Form.cs
@@ -0,0 +1,300 @@
+﻿/*************************************
+PROJECT NAME        : DevExpress 
+PROJECT INFORMATION : Using Camera Control  
+CREATE DATE         : 2021-09-24
+WRITER              : J 
+MODIFIER            : 
+MODIFY DATE         : 
+**************************************/
+using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+using DevExpress.Data.Camera;
+using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Camera;
+using DevExpress.XtraEditors.Controls;
+
+using ZXing;
+using ZXing.Common;
+
+namespace Dev014
+{
+    public partial class Form : DevExpress.XtraEditors.XtraForm
+    {
+        private List<CameraDeviceInfo> _listDeviceInfo;
+        private List<string> _listCamera;
+        private CameraDeviceInfo _info;
+        private Task _taskCheckQR;
+        private Mat _mCapture;
+        private CancellationTokenSource _cancelTokenSource;
+        private CancellationToken _qrDecodeToken;
+
+        private bool _bDecodeFinish = false;
+
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Form load event 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form_Load(object sender, EventArgs e)
+        {
+            _listCamera = new List<string>();
+            _listDeviceInfo = CameraControl.GetDevices();
+
+            if (_listDeviceInfo != null && _listDeviceInfo.Count > 0)
+            {
+                foreach (CameraDeviceInfo deviceInfo in _listDeviceInfo)
+                {
+                    CameraDevice device = CameraControl.GetDevice(deviceInfo);
+
+                    ImageComboBoxItem item = new ImageComboBoxItem();
+                    
+                    _listCamera.Add(device.Name);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Form closing event 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form_Closing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (_taskCheckQR != null && _taskCheckQR.Status == TaskStatus.Running)
+                {
+                    LF_QRDecodeCheck_Stop();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.StackTrace);
+
+            }
+        }
+
+        /// <summary>
+        /// Button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Cllick(object sender, EventArgs e)
+        {
+            switch (((SimpleButton)sender).Name)
+            {
+                case "btnCamera":
+                    MessageBox.Show("Login");
+                    LF_QRDecodeCheck_Stop();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Radio button change event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LF_Radio_CameraChange(object sender, EventArgs e)
+        {
+            try
+            {
+                string sName = string.Empty;
+
+                if (radioGroup.SelectedIndex == 1)
+                {
+                    cameraControl1.Visible = true;
+                    imageComboBoxEdit1.Visible = true;
+
+                    _info = CameraControl.GetDevices().Find(X => X.Name.Contains(_listCamera[0]));
+                    _cancelTokenSource = new CancellationTokenSource();
+                    _qrDecodeToken = new CancellationToken();
+                    _qrDecodeToken = _cancelTokenSource.Token;
+                    cameraControl1.Start(CameraControl.GetDevice(_info));
+
+                    _taskCheckQR = new Task(() => LF_QRDecodeCheck(_qrDecodeToken));
+                    _taskCheckQR.Start();
+                }
+                else
+                {
+                    cameraControl1.Visible = false;
+                    imageComboBoxEdit1.Visible = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// ComboBox index change event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LF_CameraIndex_Change(object sender, EventArgs e)
+        {
+            //Microsoft Camera Front 
+            //Microsoft Camera Rear 
+            try
+            {
+                if (imageComboBoxEdit1.SelectedIndex == 0)
+                {
+                    _info = CameraControl.GetDevices().Find(X => X.Name.Contains(imageComboBoxEdit1.SelectedText));
+                }
+                else 
+                {
+                    _info = CameraControl.GetDevices().Find(X => X.Name.Contains(imageComboBoxEdit1.SelectedText));
+                }
+                cameraControl1.Start(CameraControl.GetDevice(_info));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Changed toggle switch event 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LF_Camera_Change_Toggle(object sender, EventArgs e)
+        {
+            try
+            {
+                if (toggleSwitch1.EditValue.ToString() == "True")
+                {
+                    _info = CameraControl.GetDevices().Find(X => X.Name.Contains(_listCamera[1]));
+                }
+                else
+                {
+                    _info = CameraControl.GetDevices().Find(X => X.Name.Contains(_listCamera[0]));
+                }
+                cameraControl1.Start(CameraControl.GetDevice(_info));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Check QRDecode event 
+        /// </summary>
+        /// <param name="p_Token"></param>
+        private void LF_QRDecodeCheck(CancellationToken p_Token)
+        {
+            bool bDecodeCheck = false;
+            try
+            {
+                p_Token.ThrowIfCancellationRequested();
+
+                while (_bDecodeFinish == false)
+                {
+                    Bitmap bitmap = cameraControl1.TakeSnapshot();
+                    _mCapture = OpenCvSharp.Extensions.BitmapConverter.ToMat(bitmap);
+                    QRCodeDetector decoder = new QRCodeDetector();
+                    Point2f[] point;
+                    bDecodeCheck = decoder.Detect(_mCapture, out point);
+                    Console.WriteLine(bDecodeCheck);
+
+                    if (p_Token.IsCancellationRequested)
+                    {
+                        return;
+                    }
+                    if (bDecodeCheck)
+                    {
+                        LF_QRDecode(_mCapture);
+                    }
+                }
+            }
+            catch (AccessViolationException access)
+            {
+                Console.WriteLine(access.StackTrace);
+                Console.WriteLine(access.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// QRDecode event
+        /// </summary>
+        /// <param name="p_bitmap"></param>
+        private void LF_QRDecode(Mat p_bitmap)
+        {
+            try
+            {
+                LuminanceSource source;
+                source = new BitmapLuminanceSource(BitmapConverter.ToBitmap(p_bitmap));
+                BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(source));
+                string sSeparator = "\n";
+                Result result = new MultiFormatReader().decode(binaryBitmap);
+                if (result != null)
+                {
+                    string sDecode = result.ToString();
+                    int iIndex = sDecode.IndexOf(sSeparator);
+                    string sId = sDecode.Substring(0, iIndex);
+                    string sPwd = sDecode.Substring(iIndex + 1);
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        txtID.Text = sId;
+                        txtPwd.Text = sPwd;
+                        btnCamera.PerformClick();
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Stop QRDecode 
+        /// </summary>
+        private void LF_QRDecodeCheck_Stop()
+        {
+            try
+            {
+                if (_taskCheckQR != null && _taskCheckQR.Status == TaskStatus.Running)
+                {
+                    _cancelTokenSource.Cancel();
+                    cameraControl1.Visible = false;
+                    toggleSwitch1.Visible = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
